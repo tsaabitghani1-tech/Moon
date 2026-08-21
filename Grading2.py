9ee81a2831e79b02462ad4ddbcd70eecceb212a8
tsaabitghani1-tech <tsaabitghani1@gmail.com>
2026-08-21 11:43:20 +0700

    Add files via upload

---
+++ Grading2.py
@@ -0,0 +1,13 @@
+nilai=87
+match nilai:
+    case n if n>=90:
+        print("A")
+    case n if n>=80:
+        print("B")
+    case n if n>=70:
+        print("C")
+    case n if n>=60:
+        print("D")
+    case n if n<60:
+        print("F")
+    
